b5a25f80e531d04ffae1dba10bd1d10f37ce61eb
joaocamargo <joaovcamargo2002@gmail.com>
2022-04-04 09:15:37 -0300

    Requisito 1 JS - Selecionar itens

--- script.js
+++ script.js
@@ -1,7 +1,72 @@
+let prato;
+let bebida;
+let sobremesa;
+let valorPrato;
 
-function selecionarPrato (element) {
-    let selecao
-    selecao = element
+function selecionarPrato(prato) {
+  
+
+  const selecaoPrato = document.querySelector('.selecaoPrato') 
+  const pratoSelecionado = document.querySelector("."  + prato)
+  
+
+
+  if (selecaoPrato !== null) {
+    selecaoPrato.classList.remove('selecaoPrato') &&
+    pratoSelecionado.classList.add("selecaoPrato")
+  }
+    pratoSelecionado.classList.toggle('selecaoPrato')
+
+    atualizarBotao()
+}
+
+function selecionarBebida(bebida) {
+  
+
+  const selecaoBebida = document.querySelector('.selecaoBebida') 
+  const bebidaSelecionado = document.querySelector("."  + bebida)
+  
+
+
+  if (selecaoBebida !== null) {
+    selecaoBebida.classList.remove('selecaoBebida') &&
+    bebidaSelecionado.classList.add("selecaoBebida")
+  }
+  bebidaSelecionado.classList.toggle('selecaoBebida')
+
+  atualizarBotao()
+}
+
+function selecionarSobremesa(sobremesa) {
+  
+
+  const selecaoSobremesa = document.querySelector('.selecaoSobremesa') 
+  const sobremesaSelecionado = document.querySelector("."  + sobremesa)
+  
+
+
+  if (selecaoSobremesa !== null) {
+    selecaoSobremesa.classList.remove('selecaoSobremesa') &&
+    sobremesaSelecionado.classList.add("selecaoSobremesa")
+  }
+  sobremesaSelecionado.classList.add('selecaoSobremesa')
+
+  
+  atualizarBotao ()
+}
+
+function atualizarBotao () {
+  
+ 
+
+  const botao = document.querySelector("button")
+  const selecaoBebida = document.querySelector('.selecaoBebida')
+  const selecaoSobremesa = document.querySelector('.selecaoSobremesa')
+  const selecaoPrato = document.querySelector('.selecaoPrato')
+
+  if (selecaoBebida && selecaoSobremesa && selecaoPrato !== null) {
+    botao.classList.add("botaoVerde")
+    botao.innerHTML = "Fechar pedido"
     
-    alert("o prato selecionado foi " + selecao)
+  } 
 }
